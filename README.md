0e9c602e4564d9c2aa4514cf500f7533af414e42
Andrew Rothstein <andrew.rothstein@gmail.com>
2023-01-28 22:06:09 -0500

    arkade:0.8.60

---
+++ README.md
@@ -0,0 +1,40 @@
+andrewrothstein.arkade
+=========
+
+![Build Status](https://github.com/andrewrothstein/ansible-arkade/actions/workflows/build.yml/badge.svg)
+
+Installs [arkade](https://github.com/alexellis/arkade).
+
+Requirements
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Role Variables
+--------------
+
+See [defaults/main.yml](defaults/main.yml)
+
+Dependencies
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Example Playbook
+----------------
+
+```yml
+- hosts: servers
+  roles:
+    - andrewrothstein.arkade
+```
+
+License
+-------
+
+MIT
+
+Author Information
+------------------
+
+Andrew Rothstein <andrew.rothstein@gmail.com>
---
+++ dl-checksums.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env sh
+set -e
+DIR=~/Downloads
+MIRROR=https://github.com/alexellis/arkade/releases/download
+
+# https://github.com/alexellis/arkade/releases/download/0.8.28/arkade
+
+dl()
+{
+    local ver=$1
+    local os=$2
+    local arch=$3
+    local platform="${os}-${arch}"
+    local suffix=""
+    if [ "${os}" = "linux" ];
+    then
+        if [ "${arch}" != "amd64" ];
+        then
+            suffix="-${arch}"
+        fi
+    elif [ "${os}" = "darwin" ];
+    then
+        suffix="-${os}"
+        if [ "${arch}" != "amd64" ];
+        then
+            suffix="${suffix}-${arch}"
+        fi
+    else
+        suffix=".exe"
+    fi
+    local exe=arkade${suffix}
+    local url=$MIRROR/${ver}/${exe}.sha256
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(curl -sSL $url | awk '{print $1}')
+}
+
+dl_ver() {
+    local ver=$1
+    printf "  '%s':\n" $ver
+    dl $ver darwin amd64
+    dl $ver darwin arm64
+    dl $ver linux amd64
+    dl $ver linux arm64
+    dl $ver linux armhf
+    dl $ver windows amd64
+}
+
+dl_ver ${1:-0.8.60}
